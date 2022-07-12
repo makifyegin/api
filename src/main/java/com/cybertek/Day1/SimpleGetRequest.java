30882b8f8c279f5209bff35cd91f56c400225417
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-12 18:43:12 +0100

    i added the url from postman which is my aws machines ip addresses

--- src/main/java/com/cybertek/Day1/SimpleGetRequest.java
+++ src/main/java/com/cybertek/Day1/SimpleGetRequest.java
@@ -3,8 +3,10 @@ package com.cybertek.Day1;
 import org.junit.jupiter.api.Test;
 
 public class SimpleGetRequest {
+    String url = "http://54.227.232.110:8000/api/spartans";
+
     @Test
-    public void Test1(){
+    public void Test1() {
 
     }
 }
