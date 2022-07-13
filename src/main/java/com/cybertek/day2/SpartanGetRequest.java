0e16fb233c50256da7f45a6a1471a5c2ddf3c96e
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-13 17:34:58 +0100

    I added verify steps that assertions statusCode and contentType

--- src/main/java/com/cybertek/day2/SpartanGetRequest.java
+++ src/main/java/com/cybertek/day2/SpartanGetRequest.java
@@ -3,6 +3,7 @@ package com.cybertek.day2;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequest {
@@ -19,7 +20,23 @@ public class SpartanGetRequest {
 
         Response response = RestAssured.given().accept(ContentType.JSON)
                 .when()
-                .get(baseUrl + "api/spartans");
+                .get(baseUrl + "/api/spartans");
+
+//printing status code from response object
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing response content type from response object
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //print whole result body
+        response.prettyPrint();
+
+        //how to do API testing then?
+
+        Assertions.assertEquals(response.statusCode(), 200);
+
+        //verify content type is application/json
+        Assertions.assertEquals(response.contentType(), "application/json");
 
 
     }
