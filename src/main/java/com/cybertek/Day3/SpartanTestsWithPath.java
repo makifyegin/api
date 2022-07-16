f6b0af960574396dad5f798e282e6251113388d1
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-16 23:26:37 +0100

    How can i reach inside the bodies keys and values

---
+++ src/main/java/com/cybertek/Day3/SpartanTestsWithPath.java
@@ -0,0 +1,50 @@
+package com.cybertek.Day3;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SpartanTestsWithPath {
+
+    @BeforeAll
+    public static void init() {
+//save baseurl inside this variable so that we don't need to type each http url.
+        RestAssured.baseURI = "http://44.203.152.162:8000";
+
+    }
+
+    /*
+     Given accept type is json
+     And path param id is 10
+     When user sends a get request to "api/spartans/{id}"
+     Then status code is 200
+     And content-type is "application/json"
+     And response payload values match the following:
+          id is 10,
+          name is "Lorenza",
+          gender is "Female",
+          phone is 3312820936
+   */
+
+    @DisplayName("GET one spartan with Path Method")
+    @Test
+    public void test1() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 10)
+                .and().when().get("/api/spartans/{id}");
+
+        response.prettyPrint();
+
+        assertEquals(200,response.statusCode());
+
+
+    }
+}
