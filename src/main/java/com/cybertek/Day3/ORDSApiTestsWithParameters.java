9b78961cd5bf5e68a5b0edf7e93a906aa1420b0e
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-16 23:17:14 +0100

    i added somedifferent things

---
+++ src/main/java/com/cybertek/Day3/ORDSApiTestsWithParameters.java
@@ -0,0 +1,67 @@
+package com.cybertek.Day3;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ORDSApiTestsWithParameters {
+    @BeforeAll
+    public static void init() {
+//save baseurl inside this variable so that we don't need to type each http url.
+        RestAssured.baseURI = "http://3.93.10.240:1000/ords/hr";
+
+    }
+
+    /*
+     * Given accept type is Json
+     * And parameter:q = {"region_id":2}
+     * When users sends a GET request to "/countries"
+     * Then status code is 200
+     * And content is application/json
+     * And Payload should contain "United States of America"*/
+
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("q", "{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+        response.prettyPrint();
+    }
+
+    /*
+     * Send a GET request to employees and get only employees who work as an IT_PROG*/
+
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParams("q", "{\"job_id\": \"IT_PROG\"}")
+                .log().all()
+                .when()
+                .get("/employees");
+
+        assertEquals(200, response.statusCode());
+
+        assertEquals("application/json", response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("IT_PROG"));
+        response.prettyPrint();
+    }
+
+}
