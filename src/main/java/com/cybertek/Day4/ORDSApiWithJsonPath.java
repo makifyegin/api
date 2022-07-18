8b664a231845a2ce1fc8e64fc2a056af8d4acecd
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-18 15:37:37 +0100

    jsonPath to find something as a list

--- src/main/java/com/cybertek/Day4/ORDSApiWithJsonPath.java
+++ src/main/java/com/cybertek/Day4/ORDSApiWithJsonPath.java
@@ -1,16 +1,40 @@
 package com.cybertek.Day4;
 
 import com.cybertek.utilities.HRTestBase;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class ORDSApiWithJsonPath extends HRTestBase {
 
     @DisplayName("GET request to Countries")
     @Test
     public void test1() {
+
+        Response response = get("/countries");
+        response.prettyPrint();
+        //get the second country name with JsonPath
+
+        //to use jsonPath we assign response to JsonPAth
+        JsonPath jsonPath = response.jsonPath();
+        String secondCountryName = jsonPath.getString("items[1].country_name");
+        System.out.println("secondCountryName = " + secondCountryName);
+
+
+        //get all country ids
+
+
+        List<Object> allCountryIds = jsonPath.getList("items.country_id");
+        System.out.println(allCountryIds);
+
+
 
     }
 
