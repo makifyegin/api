7f1fc4132be9bdce4aba620bbfb490e5f6a00dd1
Junior <94207328+Makif123@users.noreply.github.com>
2022-07-18 14:03:23 +0100

    Json path

---
+++ src/main/java/com/cybertek/Day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,17 @@
+package com.cybertek.Day4;
+
+import com.cybertek.utilities.HRTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDSApiWithJsonPath extends HRTestBase {
+
+    @DisplayName("GET request to Countries")
+    @Test
+    public void test1() {
+
+    }
+
+}
